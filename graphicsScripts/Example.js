a871cf812b142a34de5f322ffc4fbaeaa3e80746
LuncyBloont <xhymaker@126.com>
2021-09-12 19:25:55 +0800

    Modify Example shader

--- graphicsScripts/Example.js
+++ graphicsScripts/Example.js
@@ -2,9 +2,12 @@ class Example extends Momo {
 	constructor() {
 		super();
 		this.mainColor = new D2dColor("#FF8945");
+		this.alpha = new D2dFloat(1);
 		
 		this.frag = `
-			return new D2dColor(tool.color); 
+			var col = new D2dColor(tool.color);
+			col.alpha = momo.alpha;
+			return col; 
 		`; // 'tool' is a object created with 'this.start' as a construct function.
 		
 		this.start = `
