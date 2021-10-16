cb3ac2c64687be81035c5d0a21e7b4495c1ed8bd
LuncyBloont <xhymaker@126.com>
2021-10-16 19:38:17 +0800

    Add 'mesh0' shader features.

--- graphicsScripts/Mesh0.js
+++ graphicsScripts/Mesh0.js
@@ -8,15 +8,23 @@ class Mesh0 extends Momo {
 		this.color = new D2dColor("#6778FF");
 		this.colorBg = new D2dColor("#acefca");
 		this.scale = new D2dRange(5, 200, 40);
-		this.tan = new D2dRange(2, 600, 20);
+		this.tan = new D2dRange(0, 600, 20);
 		this.transport = new D2dToggle(false);
+		this.double = new D2dToggle(false);
+		this.power = new D2dRange(0, 10, 1);
+		this.base = new D2dRange(0, 1, 0);
 		this.frag = `
 			let col = new D2dColor(momo.color);
 			let col2 = new D2dColor(momo.colorBg);
-			if (momo.transport) {
-				return colorLerp(col, col2, Math.cos(y * momo.scale + x * momo.tan) / 2 + 0.5);
-			}
-			return colorLerp(col, col2, Math.cos(x * momo.scale + y * momo.tan) / 2 + 0.5);
+			let p = Math.cos(y * momo.scale + x * momo.tan) / 2 + 0.5;
+
+			if (!momo.double && momo.transport) p = 1;
+			if (momo.transport || momo.double) { p *= Math.cos(-x * momo.scale + y * momo.tan) / 2 + 0.5; }
+
+			let diff = Math.abs(p - momo.base);
+			let sign = p > momo.base ? 1 : -1;
+
+			return colorLerp(col, col2, Math.pow(diff, momo.power) * sign + momo.base);
 		`;
 	}
 }
