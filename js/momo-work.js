a20f68d9ff5a02de41c3233f07c42f7842cd4538
LunvyBloont <xhymaker@126.com>
2021-11-25 10:37:52 +0800

    Add multithread support

--- js/momo-work.js
+++ js/momo-work.js
@@ -8,26 +8,42 @@ for (let i = 0; i < scriptList.length; i++) {
 onmessage = function (event) {
 	let w = event.data[0];
 	let h = event.data[1];
-	let data = event.data[2];
+	let data = new Uint8ClampedArray(w * h * 4);
 	let momos = event.data[3];
 	let momo = event.data[4];
 	let para = event.data[5];
+	let hx = event.data[7];
+	let hy = event.data[8];
+	let ex = event.data[9];
+	let ey = event.data[10];
 	let frag = new Function("x", "y", "momo", "tool", momo);
 	let start = new Function("momo", momos);
 	texLib = event.data[6];
+	let spilt = event.data[11];
 	
 	let tool = new start(para);
-	
-	for (let x = 0.0; x < w; x += 1) {
-		for (let y = 0.0; y < h; y += 1) {
-			let index = (y * w + x) * 4;
-			let col = frag(x / w, y / h, para, tool);
-			data[index] = col.R();
-			data[index + 1] = col.G();
-			data[index + 2] = col.B();
-			data[index + 3] = col.A() * 255;
+
+	for (let k = 0; k < spilt; k++) {
+		for (let v = 0; v < spilt; v++) {
+
+			let l = hx + parseInt((ex - hx) * k / spilt);
+			let t = hy + parseInt((ey - hy) * v / spilt);
+			let r = hx + parseInt((ex - hx) * (1 + k) / spilt);
+			let b = hy + parseInt((ey - hy) * (1 + v) / spilt);
+
+			for (let x = l; x < r; x += 1) {
+				for (let y = t; y < b; y += 1) {
+					let index = (y * w + x) * 4;
+					let col = frag(x / w, y / h, para, tool);
+					data[index] = col.R();
+					data[index + 1] = col.G();
+					data[index + 2] = col.B();
+					data[index + 3] = col.A() * 255;
+				}
+			}
+			postMessage([l, r, t, b, data]);
 		}
 	}
 	
-	postMessage(data);
+	
 }
--- js/momoShow.js
+++ js/momoShow.js
@@ -1,13 +1,31 @@
 let drawLoop = function () {
 	nowMomo.draw(document.getElementById("canvas"));
+	// console.log(nowMomo.rawData);
 }
 
-setInterval(drawLoop, 30);
+setInterval(drawLoop, 100);
+let workerSize = 3;
+let spiltSize = 6;
+let worker = [];
+for (let i = 0; i < workerSize; i++) {
+	for (let j = 0; j < workerSize; j++) {
+		if (!worker[i]) worker[i] = [];
+		worker[i][j] = new Worker("./js/momo-work.js");
+		worker[i][j].onmessage = function (event) {
+			console.log(event.data[0] + ", " + event.data[1] + ", " + event.data[2] + ", " + event.data[3] + " Worker complete.");
+			for (let x = event.data[0]; x < event.data[1]; x++) {
+				for (let y = event.data[2]; y < event.data[3]; y++) {
+					let index = (y * nowMomo.canvasWidth.code() + x) * 4;
+					nowMomo.rawData[index] = event.data[4][index];
+					nowMomo.rawData[index + 1] = event.data[4][index + 1];
+					nowMomo.rawData[index + 2] = event.data[4][index + 2];
+					nowMomo.rawData[index + 3] = event.data[4][index + 3];
+				}
+			}
+		};
+	}
+}
 
-let worker = new Worker("./js/momo-work.js");
-worker.onmessage = function (event) {
-	nowMomo.rawData = event.data;
-};
 
 
 function renderMomo() {
@@ -22,40 +40,34 @@ function renderMomo() {
 			let lable = document.createElement("label");
 			lable.innerHTML = i + "&emsp;";
 			let kit;
-			if (nowMomo[i].d2dFlag === "int")
-			{
+			if (nowMomo[i].d2dFlag === "int") {
 				kit = document.createElement("input");
 				kit.type = "number";
 			}
-			if (nowMomo[i].d2dFlag === "float")
-			{
+			if (nowMomo[i].d2dFlag === "float") {
 				kit = document.createElement("input");
 				kit.type = "number";
 				kit.step = "0.1";
 			}
-			if (nowMomo[i].d2dFlag === "range")
-			{
+			if (nowMomo[i].d2dFlag === "range") {
 				kit = document.createElement("input");
 				kit.type = "range";
 				kit.max = nowMomo[i].max * nowMomo[i].scale;
 				kit.min = nowMomo[i].min * nowMomo[i].scale;
 			}
-			if (nowMomo[i].d2dFlag === "color")
-			{
+			if (nowMomo[i].d2dFlag === "color") {
 				kit = document.createElement("input");
 				kit.type = "color";
 			}
-			if (nowMomo[i].d2dFlag === "toggle")
-			{
+			if (nowMomo[i].d2dFlag === "toggle") {
 				kit = document.createElement("input");
 				kit.type = "checkbox";
 			}
-			if (nowMomo[i].d2dFlag === "image")
-			{
+			if (nowMomo[i].d2dFlag === "image") {
 				kit = document.createElement("input");
 				kit.type = "file";
 			}
-			
+
 			if (kit.type && kit.type === "checkbox") {
 				kit.checked = nowMomo[i].value;
 			} else if (kit.type && kit.type === "color") {
@@ -63,11 +75,11 @@ function renderMomo() {
 			} else if (kit.type && kit.type !== "file") {
 				kit.value = nowMomo[i].value;
 			}
-			
+
 			kit.targetRoot = nowMomo;
 			kit.target = nowMomo[i];
-			
-			
+
+
 			kit.onchange = function () {
 				if (this.type && this.type === "checkbox") {
 					this.target.set(this.checked);
@@ -76,25 +88,45 @@ function renderMomo() {
 				} else {
 					this.target.set(this.value);
 				}
-				
+
+				for (let x = 0; x < nowMomo.canvasWidth.code(); x++) {
+					for (let y = 0; y < nowMomo.canvasHeight.code(); y++) {
+						let index = (y * nowMomo.canvasWidth.code() + x) * 4;
+						nowMomo.rawData[index] = 0;
+						nowMomo.rawData[index + 1] = 255;
+						nowMomo.rawData[index + 2] = 255;
+						nowMomo.rawData[index + 3] = 255;
+					}
+				}
+
 				let post = function () {
-					let obj = new Object();
-					for (let i in nowMomo) {
-						if (nowMomo[i].d2dFlag) {
-							obj[i] = nowMomo[i].code();
+					for (let i = 0; i < workerSize; i++) {
+						for (let j = 0; j < workerSize; j++) {
+							let obj = new Object();
+							for (let x in nowMomo) {
+								if (nowMomo[x].d2dFlag) {
+									obj[x] = nowMomo[x].code();
+								}
+							}
+							worker[i][j].postMessage([nowMomo.canvasWidth.code(), nowMomo.canvasHeight.code(), 0,
+							nowMomo.start, nowMomo.frag, obj, texLib,
+							parseInt(nowMomo.canvasWidth.code() * i / workerSize),
+							parseInt(nowMomo.canvasHeight.code() * j / workerSize),
+							parseInt(nowMomo.canvasWidth.code() * (i + 1) / workerSize),
+							parseInt(nowMomo.canvasHeight.code() * (j + 1) / workerSize),
+								spiltSize]);
 						}
 					}
-					worker.postMessage([nowMomo.canvasWidth.code(), nowMomo.canvasHeight.code(), nowMomo.rawData, 
-						nowMomo.start, nowMomo.frag, obj, texLib]);
 				};
-				setTimeout(post, 500);
+
+				setTimeout(post, 550);
 			};
 			/* kit.onmousemove = kit.onchange;
 			kit.onkeyup = kit.onchange;
 			kit.ontouchmove = kit.onchange; */
-			
+
 			kit.onchange();
-			
+
 			sub1.appendChild(lable);
 			sub2.appendChild(kit);
 			child.appendChild(sub1);
